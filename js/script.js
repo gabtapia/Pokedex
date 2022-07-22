a9f5f2eb8c5d346d22aa605974cb86c72aa71f51
Gabriel <gabrielnobretapia@gmail.com>
2022-07-21 22:24:44 -0300

    Função para Pesquisar Pokémons

--- js/script.js
+++ js/script.js
@@ -2,18 +2,40 @@
 const pokemonName = document.querySelector('.pokemon-name');
 const pokemonNumber = document.querySelector('.pokemon-number');
 const pokemonImage = document.querySelector('.pokemon-image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-search');
 
 // Função de Busca de Pokémons
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 // Função que Renderiza os Pokémons
 const renderPokemon = async (pokemon) => {
-    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    const data = await fetchPokemon(pokemon);
+    
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    
+        input.value = '';
+    } else {
+        pokemonName.innerHTML = 'Not Found :c';
+        pokemonNumber.innerHTML = '';
+    }
 }
+
+// Pesquisando Pokémons
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value);
+})
+
+renderPokemon('1');
